f9d5086e6b578f24f77226181053f7621c18ef81
Matthew Adesina <57835016+Himole@users.noreply.github.com>
2020-08-10 19:00:13 +0100

    add to wave 3
    lab

--- wave3 Labs/htmllist.py
+++ wave3 Labs/htmllist.py
@@ -12,6 +12,7 @@ html_str = "<ul>\n"  # "\ n" is the character that marks the end of the line, it
                      # the characters that are after it in html_str are on the next line
 
 # write your code here
-
+for str_item in items:
+    html_str = ("<li>"{}"</li>".format(str_item))
 
 print(html_str)
